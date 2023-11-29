134b8876edb079f4d80cd86302b1c8a7c87f9baf
michael <michaelngige@gmail.com>
2023-11-29 15:39:06 +0300

    dog's test passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
 
 class Dog:
-    pass
+
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+    def __init__(self, name="fido"):
+        assert 1 <= len(name) <= 25 or name == {name},"name must be string between 1 and 25 characters."
+        self.name= name
+
+    @classmethod
+    def approved_breed(cls, breed: str):
+        assert breed in cls.APPROVED_BREEDS, "Breed must be in the list of approved breeds."
+        return breed
+       
+
+        
+
+my_dog= Dog("fido")
+print(my_dog.name)
+breed_check = "Pug"
+result = Dog.approved_breed(breed_check)
+print(result)
